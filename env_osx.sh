f050ab3dda719159c98e2bec0417cbda78cf055c
Lipu Fei <lipu.fei815@gmail.com>
2018-10-11 16:37:50 +0200

    WIP: Fix mac bulid for OSX 10.9

--- env_osx.sh
+++ env_osx.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-export MACOSX_DEPLOYMENT_TARGET=10.7
-export CMAKE_OSX_DEPLOYMENT_TARGET=10.7
+
+export MACOSX_DEPLOYMENT_TARGET=10.9
+export CMAKE_OSX_DEPLOYMENT_TARGET=10.9
+export QMAKE_MACOSX_DEPLOYMENT_TARGET=10.9
+
 export CMAKE_CXX_FLAGS="-stdlib=libc++"
+export CXXFLAGS="-stdlib=libc++"
+
 export CC=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang
 export CXX=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++
-export CPATH="/usr/local/opt/openssl/include:/usr/local/opt/sqlite/include:$CPATH"
-export LIBRARY_PATH="/usr/local/opt/openssl/lib:/usr/local/opt/sqlite/lib:$LIBRARY_PATH"
-export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig:/usr/local/opt/sqlite/lib/pkgconfig:$PKG_CONFIG_PATH"
-export CXXFLAGS="-stdlib=libc++"
