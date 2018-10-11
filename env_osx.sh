86ec82c3efc085a8a5a7f11dfa5ae1b6a2a29900
Lipu Fei <lipu.fei815@gmail.com>
2018-10-11 08:24:54 +0200

    Use the same envs as in cura-build-env

--- env_osx.sh
+++ env_osx.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 export MACOSX_DEPLOYMENT_TARGET=10.7
+export CMAKE_OSX_DEPLOYMENT_TARGET=10.7
+export CMAKE_CXX_FLAGS="-stdlib=libc++"
 export CC=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang
 export CXX=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++
-export CFLAGS=-I/usr/local/include
+export CPATH="/usr/local/opt/openssl/include:/usr/local/opt/sqlite/include:$CPATH"
+export LIBRARY_PATH="/usr/local/opt/openssl/lib:/usr/local/opt/sqlite/lib:$LIBRARY_PATH"
+export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig:/usr/local/opt/sqlite/lib/pkgconfig:$PKG_CONFIG_PATH"
+export CXXFLAGS="-stdlib=libc++"
